d21089e2a4117cbfec455838012279a625804a6e
Bruno <btres76@gmail.com>
2024-02-02 14:09:13 -0300

    d

--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
 import express from 'express'
 import orderRouter from '../src/routes/ordersRouter.js'
 import cors from 'cors'
-const app = express()
+import serverless from "serverless-http";
 
+
+const app = express()
 const PORT = process.env.PORT || 3030
 
-app.use("/", orderRouter)
 app.use(express.json())
 app.use(cors())
+app.use("/orders", orderRouter)
+
+app.use('/test', (req, res) => {
+    res.send("running fine")
+})
 
 app.listen(PORT, (req, res) => {
     return console.log("server running on port " + PORT)
 })
 
-export default app
+export const handler = serverless(app);
+
